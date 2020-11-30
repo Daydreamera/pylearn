6e4de30de31d4991d2dcb40e28333c039c7d92a7
cong.wang <cong.wang@datayes.com>
2020-11-30 13:52:39 +0800

    new export_by_day.py

--- export_csv/export_by_day.py
+++ export_csv/export_by_day.py
@@ -7,90 +7,79 @@ import time
 import json
 import pymssql
 import datetime
+import pandas as pd
 from DBUtils.PooledDB import PooledDB
 
 pool_args = (0, 0, 0, 10, True, 0)
 db_args = json.loads(
-    """{"host":"sh-dm-db04-r0.datayes.com","user":"uts_sync","password":"uts_sync","database":"datayesdb","charset":"utf8"}""",
+    """{"host":"10.21.139.73","user":"uts_sync","password":"uts_sync","database":"datayesdb","charset":"utf8"}""",
     encoding='utf-8')
 conn_pool = PooledDB(pymssql, *pool_args, **db_args)
 
-
+dir_path = '/51_datacopy/to_jiaojiao/DCPM-59253'
 # begin_date_str = '20190901'
 # end_date_str = '20200902'
 
 
-def get_dateRange_list(begin_date_str, end_date_str):
-    begin_date = datetime.datetime.strptime(begin_date_str, '%Y%m%d')
-    end_date = datetime.datetime.strptime(end_date_str, '%Y%m%d')
-    # print(begin_date)
+def str_to_date(date_str):
+    date_fmt = datetime.datetime.strptime(date_str, '%Y%m%d')
+    return date_fmt
 
-    date_diff = (end_date - begin_date).days
-    date_list = []
-    for i in range(date_diff):
-        tmp = begin_date + datetime.timedelta(i)
-        date_str = str(tmp.strftime('%Y%m%d'))
-        # print(date_str)
-        date_list.append(date_str)
-    return date_list
-
+def date_to_str(date_fmt):
+    return date_fmt.strftime('%Y%m%d')
 
 def export_main(begin_date_str, end_date_str):
     conn = conn_pool.connection()
-    cur = conn.cursor()
-    date_list = get_dateRange_list(begin_date_str, end_date_str)
-    for i in range(len(date_list)):
+    begin_date = str_to_date(begin_date_str)
+    end_date = str_to_date(end_date_str)
+    while begin_date < end_date:
+        next_day = begin_date + datetime.timedelta(1)
         sql = """
-        select 
-        UPDATE_TIME,
-        TRADE_CD,
-        cast(TMSTAMP as bigint) TMSTAMP,
-        TICKET_CODE,
-        SHC_NAME,
-        SHC_ID,
-        PARTY_NAME,
-        ID,
-        HOLD_VOL,
-        HOLD_PCT,
-        END_DATE,
-        ADDRESS,
-        SECURITY_ID 
-        from hk_shsz_detl with(nolock) where END_DATE >= '{}' and END_DATE < '{}'
-        """.format(date_list[i], date_list[i + 1])
-        cur.execute(sql)
-        file_name = r'/51_datacopy/to_jiaojiao/DCPM56319_20200916' + '//hk_shsz_detl' + date_list[i] + '.csv'  # 生成文件名
+            select 
+            ID,
+            SECURITY_ID,
+            TICKER_SYMBOL,
+            EXCHANGE_CD,
+            TRADE_DATE,
+            PER_CASH_DIV,
+            PER_SHARE_DIV_RATIO,
+            PER_SHARE_TRANS_RATIO,
+            ALLOTMENT_RATIO,
+            ALLOTMENT_PRICE,
+            OPEN_PRICE,
+            HIGHEST_PRICE,
+            LOWEST_PRICE,
+            CLOSE_PRICE,
+            PRE_CLOSE_PRICE_2,
+            OPEN_PRICE_2,
+            HIGHEST_PRICE_2,
+            LOWEST_PRICE_2,
+            CLOSE_PRICE_2,
+            TURNOVER_VOL,
+            ACCUM_ADJ_FACTOR_2,
+            UPDATE_TIME
+            from mkt_equd_adj_af with(nolock)  
+            where TRADE_DATE>='{}' 
+            and TRADE_DATE<'{}'
+            order by TICKER_SYMBOL
+        """.format(begin_date, next_day)
+        file_name = 'mkt_equd_adj_af-' + date_to_str(begin_date) + '.csv'  # 生成文件名
+        file_path = os.path.join(dir_path,file_name)
         # 文件若存在 则删除
-        if os.path.exists(file_name):
-            os.remove(file_name)
-        # 新建目标文件
-        f = open(file_name, 'a', encoding='utf8')
-        # 写入列名
-        columns = []
-        for column in cur.description:
-            columns.append(column[0])
-        f.write('#@DatayesCol@#'.join(columns))
-        f.write('#@DatayesRow@#')
-
-        print(date_list[i] + ' strat to write:', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-        # 按行抓取数据
-        data = cur.fetchone()
-        count = 0
-        while data is not None:
-            data_to_str = []
-            for da in data:
-                data_to_str.append(str(da))
-            f.write('#@DatayesCol@#'.join(data_to_str))
-            f.write('#@DatayesRow@#')
-            data = cur.fetchone()
-            count += 1
-        f.close()
-        print('The number of dataRow is :', count)
-        print(date_list[i] + ' is write over:', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
-    cur.close()
+        if os.path.exists(file_path):
+            os.remove(file_path)
+        df = pd.read_sql(sql, conn)
+        if df is not None and len(df) > 0:
+            print(date_to_str(begin_date) + ' strat to write:', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+            df.to_csv(file_path, encoding='utf-8', index=False)
+            print(date_to_str(begin_date) + ' is write over:', time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()))
+            begin_date = next_day
+        else:
+            begin_date = next_day
+            continue
     conn.close()
 
 
 if __name__ == '__main__':
-    export_main('20180101', '20190901')
 
-    # print(get_dateRange_list('20190901', '20191004'))
+    export_main('20180101', '20180110')
